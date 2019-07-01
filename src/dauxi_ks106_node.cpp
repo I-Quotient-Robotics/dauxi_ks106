7a8061f7045ceea4e4186e7d59c5f846831c2e12
Ye Tian <flymaxty@foxmail.com>
2019-07-01 17:39:59 +0800

    change package and node name, fix incorrect code style

--- src/dauxi_ks106_node.cpp
+++ src/dauxi_ks106_node.cpp
@@ -4,16 +4,20 @@
 int main(int argc,char** argv) {
 
   ros::init(argc,argv,"ks106_node");
-  ros::NodeHandle n("~");
-  IQR::Ks106Uart ks106(n);
+  ros::NodeHandle private_nh("~");
+  IQR::Ks106Uart ks106(private_nh);
+
+  //declare the uart port
   while(ks106.UartInit() == false) {
-    sleep(1);          //declare the uart port
+    sleep(1);
   }
-  //add check code ,set ks106 with launch setting
+
+  //add check code, set ks106 with launch setting
   ros::Rate loop_rate(ks106.Frequency());
   while(ros::ok()) {
     ks106.Ks106Run();
     loop_rate.sleep();
   }
+
   return 0;
 }
